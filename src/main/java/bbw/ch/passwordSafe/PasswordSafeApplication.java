95da00388ad828b9fec678d2733ae985d7103fb9
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-06-22 11:25:36 +0200

    Add function to start application

--- src/main/java/bbw/ch/passwordSafe/PasswordSafeApplication.java
+++ src/main/java/bbw/ch/passwordSafe/PasswordSafeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PasswordSafeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(PasswordSafeApplication.class, args);
 	}
-
 }

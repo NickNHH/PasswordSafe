56b6f4527ac74d90a2eaccfefaad30a0e078b1a8
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-06-28 16:29:50 +0200

    Add safer encryption

--- src/main/java/bbw/ch/passwordSafe/EntryController.java
+++ src/main/java/bbw/ch/passwordSafe/EntryController.java
@@ -7,13 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 @Controller
 public class EntryController {
     @Autowired
     private EntryList entryList;
 
+    private String pSalt = "AES-GSM Password-Bases encryption!";
+
     @GetMapping("/list-entries")
     public String listEntries(Model model) {
+        for (Entry entry : entryList.getAllEntries()) {
+            try {
+                String decryptedPW = Encryptor.decrypt(entry.getPassword(), pSalt);
+                entry.setPassword(decryptedPW);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         model.addAttribute("listOfEntries", entryList.getAllEntries());
         model.addAttribute("newEntry", new Entry());
         return "entrylist";
@@ -21,6 +33,12 @@ public class EntryController {
 
     @PostMapping("/add-entry")
     public String addEntry(@ModelAttribute Entry newEntry, Model model) {
+        try {
+            String encryptedPW = Encryptor.encrypt(newEntry.getPassword().getBytes(UTF_8), pSalt);
+            newEntry.setPassword(encryptedPW);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         entryList.addEntry(newEntry);
         model.addAttribute("listOfEntries", entryList.getAllEntries());
         return "redirect:/list-entries";
@@ -33,5 +51,18 @@ public class EntryController {
         }
         model.addAttribute("listOfEntries", entryList.getAllEntries());
         return "redirect:/list-entries";
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
+        for (Entry entry : entryList.getAllEntries()) {
+            try {
+                String encryptedPW = Encryptor.encrypt(entry.getPassword().getBytes(UTF_8), pSalt);
+                entry.setPassword(encryptedPW);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return "redirect:/login?logout";
     }
 }
